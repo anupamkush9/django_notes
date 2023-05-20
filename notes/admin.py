0ea8bad249efb2b7f74805697bd813fbfc758397
Anupamkush9 <anupam.kush9@gmail.com>
2023-05-20 12:05:30 +0530

    basic set up

--- notes/admin.py
+++ notes/admin.py
@@ -1,25 +1,25 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 
-# class BookAdmin(admin.ModelAdmin):
-#     list_display = ['photo', 'book_name']
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['photo', 'book_name']
 
-# class FilesAdmin(admin.ModelAdmin):
-#         class Media:
-#             js = (
-#                 'https://code.jquery.com/jquery-3.6.1.min.js',
-#                 'js/FilesAdmin.js',  # project static folder
-#             )
+class FilesAdmin(admin.ModelAdmin):
+        class Media:
+            js = (
+                'https://code.jquery.com/jquery-3.6.1.min.js',
+                'js/FilesAdmin.js',  # project static folder
+            )
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ['name', ]
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['name', ]
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
 
-# # Register your models here
-# admin.site.register(Files, FilesAdmin)
-# admin.site.register(Temp_Data_Bulk_Create)
-# admin.site.register(Book, BookAdmin)
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Product, ProductAdmin)
+# Register your models here
+admin.site.register(Files, FilesAdmin)
+admin.site.register(Temp_Data_Bulk_Create)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
