b163fab56127934c5621d82883f0020cd32248a9
Anupamkush9 <anupam.kush9@gmail.com>
2024-07-13 08:24:01 +0530

    commented duplicate FilesAdmin

--- notes/admin.py
+++ notes/admin.py
@@ -18,12 +18,12 @@ class FilesAdmin(admin.ModelAdmin):
 # class BookAdmin(admin.ModelAdmin):
 #     list_display = ['photo', 'book_name']
 
-class FilesAdmin(admin.ModelAdmin):
-        class Media:
-            js = (
-                'https://code.jquery.com/jquery-3.6.1.min.js',
-                'js/FilesAdmin.js',  # project static folder
-            )
+# class FilesAdmin(admin.ModelAdmin):
+#         class Media:
+#             js = (
+#                 'https://code.jquery.com/jquery-3.6.1.min.js',
+#                 'js/FilesAdmin.js',  # project static folder
+#             )
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', ]
